3af02dd10c1bf66210be17a8c1aa06a0b47ed311
Alkagar <alkagar@gmail.com>
2012-11-17 21:16:27 +0100

    add global options properties

--- ADebugConfig.php
+++ ADebugConfig.php
@@ -1,12 +1,14 @@
 <?php
 
-    class ADebugUtil
+    class ADebugConfig
     {
         public static $enabled = false;
 
         public static $enabledCli = false;
 
         public static $enabledWeb = false;
+
+        private static $options = null; 
 
         public static function isCli()
         {
@@ -46,5 +48,26 @@
         public static function disableWeb()
         { 
             self::$enabledWeb = false;
+        }
+
+        private static function _initializeOptions()
+        {
+            if(is_null(self::$options)) {
+                self::$options = new StdClass();
+                self::$options->enabled = self::$enabled;
+                self::$options->enabledCli = self::$enabledCli;
+                self::$options->enabledWeb = self::$enabledWeb;
+            }
+        }
+
+        public static function config($optionName, $optionValue)
+        {
+            self::_initializeOptions();
+            if(isset(self::$options->$optionName)) {
+                self::$options->$optionName = $optionValue;
+                if(isset(self::$optionName)) {
+                    self::$optionName = $optionValue;
+                }
+            }
         }
     }
