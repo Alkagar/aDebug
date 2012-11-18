5b87dc79a9821654821646065856e08c1cc48013
Alkagar <alkagar@gmail.com>
2012-11-18 08:50:23 +0100

    add simple cli var_dump

--- ADebug.php
+++ ADebug.php
@@ -1,11 +1,106 @@
 <?php
     class ADebug
     {
+        private static $_level = 0;
+
+
+        private static function _iterateOverElements($element, $elementName)
+        {
+            if(is_object($element)) {
+                self::_printObject($element, $elementName);
+                self::_iterateOverObject($element);
+                return false;
+            }
+            if(is_array($element)) {
+                self::_printArray($element, $elementName);
+                self::_iterateOverArray($element);
+                return false;
+            }
+            return true;
+        }
+
+        private static function _getTabulator()
+        {
+            $i = 0;
+            $tabulator = '';
+            while($i++ < self::$_level) {
+                $tabulator .= '   ';
+            }
+            return $tabulator;
+        }
+
+        private static function _iterateOverObject($element)
+        {
+            self::$_level++;
+            $reflect = new ReflectionClass($element);
+            //by default get all properties, add some parameters in config to change this
+            $properties   = $reflect->getProperties();
+            $allProperties = array();
+
+            foreach ($properties as $property) {
+                $propertyName = $property->getName();
+                $property->setAccessible(true);
+                $allProperties[$propertyName] = $property->getValue($element);
+            } 
+            foreach($element as $propertyName => $propertyValue) {
+                if(!isset($allProperties[$propertyName])) {
+                    $allProperties[$propertyName] = $propertyValue;
+                }
+            }
+            foreach($allProperties as $propertyName => $propertyValue) {
+                if(self::_iterateOverElements($propertyValue, $propertyName)) {
+                    self::_printPrimitive($propertyValue, $propertyName);
+                }
+            }
+            self::$_level--;
+        }
+
+        private static function _iterateOverArray($element)
+        {
+            self::$_level++;
+            foreach($element as $elementKey => $subElement) {
+                if(self::_iterateOverElements($subElement, $elementKey)) {
+                    self::_printPrimitive($subElement, $elementKey);
+                }
+            } 
+            self::$_level--;
+        }
+
+        private static function _printPrimitive($element, $elementName)
+        {
+            echo 
+            self::_getTabulator() . 
+            $elementName . ' => ' .
+            '(' . gettype($element) . ') ' .
+            $element .
+            PHP_EOL;
+        }
+
+        private static function _printArray($element, $elementName) 
+        {
+            echo 
+            self::_getTabulator() . 
+            $elementName . ' => ' .
+            '(' . gettype($element) . ')' .
+            ': ' .
+            PHP_EOL; 
+        }
+
+        private static function _printObject($element, $elementName) 
+        {
+            echo 
+            self::_getTabulator() . 
+            $elementName . ' => ' .
+            '(' . get_class($element) . ')' .
+            ': ' .
+            PHP_EOL; 
+        }
+
         public static function dump($var)
         {
             $args = func_get_args();
-            foreach($args as $arg) {
-                var_dump($arg);
+            foreach($args as $key => $arg) {
+                self::_iterateOverElements($arg,$key);
             }
         }
 
